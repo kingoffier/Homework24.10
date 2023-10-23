8471b5fb761c5627cb969bbf8d5ebbafe3e38c08
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-10-23 22:40:03 +0500

    Add files via upload

---
+++ Ex7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToLower();
+            string pattern = "0123456789";
+            string result = string.Empty;
+            foreach (char item in str)
+            {
+                if (Char.IsDigit(item))
+                {
+                    if (!result.Contains(item))
+                    {
+                        result += item;
+                    }
+                }
+
+            }
+            foreach (char item in result)
+            {
+
+                pattern = pattern.Replace(item.ToString(), string.Empty);
+
+            }
+            if(pattern == string.Empty)
+            {
+                pattern = "NO";
+            }
+            Console.WriteLine(pattern);
+        }
+    }
+}
