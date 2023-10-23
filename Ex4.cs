7718a4587e2590d1a6216662ebc2cf794cff1590
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-10-23 11:45:42 +0500

    Add files via upload

---
+++ Ex4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string number = string.Empty;
+            string result = string.Empty;
+            foreach (char item in str)
+            {
+                if (!result.Contains(item))
+                {
+                    result += item;
+                }
+            }
+            foreach (char item in result)
+            {
+                int count = 0;
+                foreach (char symbol in str)
+                {
+                    if (item == symbol)
+                    {
+                        count++;
+                    }
+                }
+                if (count > 1 && Char.IsDigit(item))
+                {
+                    number += item;
+                }
+
+            }
+            if (number == string.Empty)
+            {
+                number = "NO";
+            }
+        
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+    }
+}
