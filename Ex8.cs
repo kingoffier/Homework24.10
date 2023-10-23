18683a7f72acf8b9df6b8bbbc33592226c3080dc
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-10-23 23:31:41 +0500

    Add files via upload

---
+++ Ex8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToLower();
+            string str2 = Console.ReadLine().ToLower();
+            string xy = str + str2;
+            string pattern = "abcdefghijklmnopqrstuvwxyz";
+            string result=string.Empty;
+            foreach (char item in xy)
+            {
+                if (Char.IsLetter(item))
+                {
+                    if(!result.Contains(item))
+                    {
+                        result += item;
+                    }
+                }
+            }
+            foreach(char item in result)
+            {
+                pattern = pattern.Replace(item.ToString(), string.Empty);
+            }
+            if(pattern == string.Empty)
+            {
+                pattern = "()";
+            }
+           
+            Console.WriteLine(pattern.ToUpper());
+        }
+    }
+}
