29b3cae6a79ee106278c02a8cb6a01a2796d6d3d
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-10-23 11:47:10 +0500

    Add files via upload

---
+++ Ex5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string pattern = "abcdefghijklmnopqrstuvwxyz0123456789_";
+            str = str.ToLower();
+            if (Char.IsDigit(str.First()))
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
---
+++ Ex6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str=Console.ReadLine();
+            string result=string.Empty;
+            int i = result.Length;
+            foreach(char item in str)
+            {
+                if (!result.Contains(item))
+                {
+                    result += item;
+                    i++;
+                }
+            }
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
